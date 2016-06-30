7674c9f3dcb0388aa8fbc0c70807d76df226e88c
wqliang <384598740@qq.com>
2016-06-30 23:59:20 +0800

    338 counting bits

---
+++ leetcode/338_counting_bits.h
@@ -0,0 +1,35 @@
+#ifndef  __SOLUTION_H__
+#define  __SOLUTION_H__
+#include"Solution.h"
+#endif
+#include<vector>
+
+class Solution338 : public Solution{
+public:
+	vector<int> countBitws(int num);
+	void run();
+};
+
+vector<int> Solution338::countBitws(int num){
+	vector<int> v = vector<int>(num+1);
+	v[0] = 0;
+	for (int i = 1; i <= num; i++){
+		v[i] = v[i/2]+(i%2);
+	}
+	return v;
+}
+
+void Solution338::run(){
+	int num;
+	cout << "EOF to end\n";
+	while (cin >> num){
+		vector<int> ans = this->countBitws(num);
+		cout << "[";
+		for (int i = 0; i < ans.size(); i++){
+			if (i == ans.size()-1)
+				cout << ans[i] << "]\n";
+			else
+				cout << ans[i] << ", ";
+		}
+	}
+}
--- leetcode/Main.cpp
+++ leetcode/Main.cpp
@@ -1,55 +1,14 @@
 #include<iostream>
 #include"342_Power_of_Four.h"
 #include"344_reverse_string.h"
+#include"338_counting_bits.h"
 
 int main(){
 	//Solution* s = new Solution342();
-	Solution* s = new Solution344();
+	//Solution* s = new Solution344();
+	Solution* s = new Solution338();
 	s->run();
-	/*
-	Solution300 s;
-	int a[] = {1,2,-10,-8,-7};
-	vector<int> v(a, a+8);
-	cout << s.increasingTriplet(v);
-	*/
-	/*  //338
-	int num;
-	cin >> num;
-	vector<int> ans = s.countBitws(num);
-	for (int i = 0; i < ans.size(); i++){
-		cout << ans[i] << "\n";
-	}
-	*/
-	/*335
-	int n;
-	cin >> n;
-	vector<int> v = vector<int>(n);
-	for (int i = 0; i < n; i++)
-		cin >> v[i];
-	cout << s.isSelfCrossing(v);
-	*/
-	/*
-	345
-	Solution345 s;
-	cout << s.reverseVowels("hello") << endl;
-	cout << s.reverseVowels("leetcode") << endl;
-	cout << s.reverseVowels("") << endl;
-	cout << s.reverseVowels("aeiou") << endl;
-	cout << s.reverseVowels("bcdfghjk") << endl;
-	cout << s.reverseVowels("a") << endl;
-	cout << s.reverseVowels("b") << endl;
-	*/
-
-	/*344*//*
-	Solution344 s;
 	
-	*/
-	/*
-	Solution343 s;
-	cout << s.integerBreak(2) << endl;
-	cout << s.integerBreak(10) << endl;
-	cout << s.integerBreak(99) << endl;
-	*/
 	system("pause");
 	return 0;
 }
